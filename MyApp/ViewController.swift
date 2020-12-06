111f9099d4f989a89f57157ff036bd8c50764d3d
tako <javadava.58.pts@gmail.com>
2020-12-06 22:44:01 +0900

    Hello World!

---
+++ MyApp/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  MyApp
+//
+//  Created by isobe-d on 2020/12/06.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBAction func TapGreen(_ sender: Any) {
+        view.backgroundColor = UIColor.green
+    }
+    
+    @IBAction func TopWhite(_ sender: Any) {
+        view.backgroundColor = UIColor.white
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
