773b3e4f5b5bdcd55dba9ebbc3833edeb85b0416
tako <javadava.58.pts@gmail.com>
2020-12-10 00:41:53 +0900

    変数名変更

--- Playground/castOperator.playground/Contents.swift
+++ Playground/castOperator.playground/Contents.swift
@@ -1,12 +1,12 @@
 import UIKit
 
 func calc(unitPrice:Int, quantity:Int) -> Int {
-    let totalPrice = unitPrice * quantity
-    return totalPrice
+    let sum = unitPrice * quantity
+    return sum
 }
 
-let totalPrice = calc(unitPrice: 250, quantity: 5)
+let subtotal = calc(unitPrice: 250, quantity: 5)
 
-let tax = 0.1
+let taxRate = 0.1
 
-let priceIncludingTax = Double(totalPrice) * (1 + tax)
+let totalPrice = Double(subtotal) * (1 + taxRate)
