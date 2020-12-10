4951bb1bf9016892d54d38ebdd6d1d08a010b32e
tako <javadava.58.pts@gmail.com>
2020-12-10 20:51:44 +0900

    修正

--- Playground/statment1.playground/Contents.swift
+++ Playground/statment1.playground/Contents.swift
@@ -2,7 +2,7 @@ import UIKit
 
 let msg1 = "你好"
 let msg2 = "我好"
-let msg3 = "他家好"
+let msg3 = "大家好"
 let msg4 = "Hello!"
 // terminator: を指定した場合は改行されない。
 print(msg1, msg2, msg3, separator: "、", terminator:"!")
