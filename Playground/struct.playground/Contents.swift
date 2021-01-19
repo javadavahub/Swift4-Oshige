adedc60d5133717ec0e77aa49319ce13325880ce
tako <javadava.58.pts@gmail.com>
2021-01-19 23:59:12 +0900

    struct

---
+++ Playground/struct.playground/Contents.swift
@@ -0,0 +1,50 @@
+import UIKit
+
+/**
+ 構造体
+ SwiftUIで多く使われるなど、クラスよりもAppleに推されている
+ クラスとの違いは、構造体のインスタンスが配列に複数格納できること
+ struct 構造体名 {
+   プロパティ、メソッド...
+ }
+ 
+ インスタンス
+ var インスタンス = 構造体名()
+ */
+
+//struct Animal {
+//    let age = 5
+//    var kind = "犬"
+//
+//    func Bite() -> () {
+//        print("\(age)歳の\(kind)が噛み付く")
+//    }
+//}
+//
+//var dog = Animal()
+//print(dog.age)
+//dog.Bite()
+
+struct Animal {
+    let age: Int
+    var kind: String
+    
+    func Bite() -> () {
+        print("\(age)歳の\(kind)が噛み付く")
+    }
+    
+    init(age: Int, kind: String) {
+        self.age = age
+        self.kind = kind
+    }
+}
+
+var dog = Animal(age: 1, kind: "犬")
+dog.Bite()
+
+var animals:[Animal] = [
+    Animal(age: 8, kind: "虎"),
+    Animal(age: 7, kind: "ワニ")
+]
+
+animals[1].Bite()
